556c68434dfbf346b1381b8d1f5d983fa2738960
esselmanc123 <cse94833@uga.edu>
2024-06-11 20:41:37 -0400

    Used Codieum to make least squares

--- Least_squares/Esselman_Least_squares.cpp
+++ Least_squares/Esselman_Least_squares.cpp
@@ -7,14 +7,26 @@
 #include <cmath>
 #include <cstdlib>
 #include <time.h>
+#include "/Users/christopheresselman/vscode_header_files/eigen-3.4.0/Eigen/Dense"
 #include <sstream>
 
 using namespace std;
 using namespace Eigen;
 
+// Codeium auto completion for C++
+
 //Function to read in CSV and put into an Eigen matrix I got from here:
 // https://www.youtube.com/watch?v=K9QB1LbemnY
 
+/*
+ * Reads data from a CSV file and returns it as an Eigen Matrix.
+ *
+ * @param fileToOpen The path to the CSV file to be opened.
+ *
+ * @return A MatrixXd object containing the data from the CSV file.
+ *
+ * @throws None.
+ */
 MatrixXd openData(string fileToOpen)
 {
 	//Vector to map to eigenmatrix
@@ -68,6 +80,21 @@ int main(int argc, char **argv)
 	//Use the function to read in the csv file
     MatrixXd points = openData(csv_file_name);
 
-	cout << points << "\n";
+	//Make column matrix that is the second column of points
+	MatrixXd b = points.col(1);
+
+	//Make a 2 column matrix where the first column is all ones and the second column is the first column of points
+	MatrixXd A = MatrixXd::Ones(points.rows(),2);
+	A.col(1) = points.col(0);
+
+	//Print to the terminal b and A
+	cout << "b = \n" << b << "\n";
+	cout  << "A = \n" << A << "\n";
+
+	cout << "points = \n"  << points << "\n";
+
+	//Solve the least squares problem
+	MatrixXd x = (A.transpose()*A).inverse()*A.transpose()*b;
+	cout << "x = \n" << x << "\n";
     return 0;
 }
